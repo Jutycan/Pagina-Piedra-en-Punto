a6038db33af689487c15ebd19836b68a0298ff49
Julian Cortes <cortes270k@gmail.com>
2025-09-01 16:00:41 -0500

    se sigue trabajando en las 6 internas de servicios

--- js/internas-servicios/estudios-del-consumidor.js
+++ js/internas-servicios/estudios-del-consumidor.js
@@ -20,3 +20,60 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+// ----------------------------------------------------------------------------- 
+//----------------------Estilos de faqs-uno------------------------------------- 
+// ----------------------------------------------------------------------------- 
+document.querySelectorAll(".faq-uno-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const faqItem = button.parentElement;
+
+        // Cierra todos los bloques antes de abrir el nuevo
+        document.querySelectorAll(".faq-uno-item").forEach(item => {
+            if (item !== faqItem) {
+                item.classList.remove("active");
+            }
+        });
+
+        // Alternar el estado del clic
+        faqItem.classList.toggle("active");
+    });
+});
+
+// ----------------------------------------------------------------------------- 
+//----------------------Estilos de faqs-dos------------------------------------- 
+// -----------------------------------------------------------------------------
+document.querySelectorAll(".faq-dos-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const faqItem = button.parentElement;
+
+        // Cierra todos los bloques antes de abrir el nuevo
+        document.querySelectorAll(".faq-dos-item").forEach(item => {
+            if (item !== faqItem) {
+                item.classList.remove("active");
+            }
+        });
+
+        // Alternar el estado del clic
+        faqItem.classList.toggle("active");
+    });
+});
+
+// ----------------------------------------------------------------------------- 
+//----------------------Estilos de faqs-tres------------------------------------- 
+// -----------------------------------------------------------------------------
+document.querySelectorAll(".faq-tres-question").forEach(button => {
+    button.addEventListener("click", () => {
+        const faqItem = button.parentElement;
+
+        // Cierra todos los bloques antes de abrir el nuevo
+        document.querySelectorAll(".faq-tres-item").forEach(item => {
+            if (item !== faqItem) {
+                item.classList.remove("active");
+            }
+        });
+
+        // Alternar el estado del clic
+        faqItem.classList.toggle("active");
+    });
+});
